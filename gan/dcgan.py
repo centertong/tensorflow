8f0b8f60303e1827e46b04d5250af8e5d8a8343e
centertong <centertong@gmail.com>
2017-05-19 18:25:38 +0900

    add three types of cost function for GAN

--- gan/dcgan.py
+++ gan/dcgan.py
@@ -4,7 +4,10 @@ from gan.network import Network
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-z_dim = 100
+import os
+import cv2
+
+z_dim = 200
 
 class DCGAN(Network):
     def __init__(self, X_size, Y_size , trainable=True, name=None):
@@ -20,8 +23,8 @@ class DCGAN(Network):
     def setup(self):
         #generator
         (self.feed('dataZ')
-         .reshape([-1,1,1,100], name='gen_reshape')
-         .deconv(4,4, 1024,1,1, name='gen_deconv1')
+         .reshape([-1,1,1,200], name='gen_reshape')
+         .deconv(1,1, 1024,4,4, name='gen_deconv1', padding='VALID')
          .deconv(5,5, 512, 2,2, relu= False, name='gen_deconv2')
          .bn(512, name='gen_deconv2_bn')
          .relu(name='gen_deconv2_relu')
@@ -49,7 +52,7 @@ class DCGAN(Network):
          .conv(5, 5, 1024, 2, 2, relu= False, name='dis_conv4')
          .bn(1024, name='dis_conv4_bn')
          .lrelu(name='dis_conv4_lrelu')
-         .conv(4,4, 1, 4, 4, relu= False, name='dis_prob')
+         .conv(4,4, 1,1, 1, relu= False, name='dis_prob', padding='VALID')
          .sigmoid(name='real_dis'))
 
         (self.feed('gen_prob')
@@ -64,20 +67,39 @@ class DCGAN(Network):
          .conv(5, 5, 1024, 2, 2, relu= False, name='dis_conv4', reuse=True)
          .bn(1024, name='dis_conv4_bn')
          .lrelu(name='dis_conv4_lrelu')
-         .conv(4, 4, 1, 4, 4, relu= False, name='dis_prob', reuse=True)
+         .conv(4, 4, 1, 1,1, relu= False, name='dis_prob', reuse=True, padding='VALID')
          .sigmoid(name='gen_dis'))
 
 
-from tensorflow.examples.tutorials.mnist import input_data
-import os
 
-mnist = input_data.read_data_sets("../dataset/MNIST/", one_hot=True)
+def read_dataset(path):
+    file_list = os.listdir(path)
+    dataset = []
+    for file in file_list:
+        if file.split('.')[-1] == 'png':
+            img = cv2.imread(os.path.join(path, file))
+            w,h,c = img.shape
+            wp = 0
+            hp = 0
+            if w % 64:
+                wp = 1
+            if h % 64:
+                hp = 1
+
+            for i in range(w // 64):
+                for j in range(h // 64):
+                    tmp = img[i * (64 + wp): i * (64 + wp) + 64, j * (64 + hp): j * (64 + hp) + 64]
+                    dataset.append(tmp)
+    return np.array(dataset)
+
+
+dataset = read_dataset('../dataset/pokemon/GenI')
 
 learning_rate = 0.001
-training_iters = 10000
-batch_size = 32
+training_iters = 500
+batch_size = 10
 display_step = 10
-d_steps = 3
+d_steps = 1
 
 def sample_z(m, n):
     return np.random.uniform(-1., 1., size=[m, n])
@@ -100,15 +122,15 @@ def plot(samples):
 
 def train(net, sess):
     sess.run(init)
-    if os.path.exists("../learn_param/mnist_gan.ckpt.meta"):
-        net.load("../learn_param/mnist_gan.ckpt", sess, True)
+    if os.path.exists("../learn_param/dcgan.ckpt.meta"):
+        net.load("../learn_param/dcgan.ckpt", sess, True)
 
-    step = 1
+    step = 0
     i = 0
     while step < training_iters:
         D_loss_curr = None
         for _ in range(d_steps):
-            X_mb, _ = mnist.train.next_batch(batch_size)
+            X_mb = dataset[step * batch_size: (step+1) * batch_size]
             z_mb = sample_z(batch_size, z_dim)
 
             _, D_loss_curr = sess.run(
@@ -116,7 +138,7 @@ def train(net, sess):
                 feed_dict={net.dataX: X_mb, net.dataZ: z_mb}
             )
 
-        X_mb, _ = mnist.train.next_batch(batch_size)
+        X_mb = dataset[step * batch_size: (step+1) * batch_size]
         z_mb = sample_z(batch_size, z_dim)
 
         _, G_loss_curr = sess.run(
@@ -124,6 +146,7 @@ def train(net, sess):
             feed_dict={net.dataX: X_mb, net.dataZ: sample_z(batch_size, z_dim)}
         )
 
+        step += 1
         if step % 1000 == 0:
             print('Iter: {}; D_loss: {:.4}; G_loss: {:.4}'
                   .format(step, D_loss_curr, G_loss_curr))
@@ -136,15 +159,15 @@ def train(net, sess):
             i += 1
             plt.close(fig)
 
-        step += 1
+
     save_path = net.saver.save(sess, "../learn_param/dcgan.ckpt")
     print("Model saved infile : {}".format(save_path))
 
 if __name__ == '__main__':
-    net = DCGAN(X_size=[28,28,3], Y_size=[z_dim], name='dcgan')
+    net = DCGAN(X_size=[64,64,3], Y_size=[z_dim], name='dcgan')
 
-    gen_var = net.get_variables('gen_fc') + net.get_variables('gen_log_prob')
-    dis_var = net.get_variables('dis_fc') + net.get_variables('dis_prob')
+    gen_var = net.get_variables('gen_deconv1') + net.get_variables('gen_deconv2') + net.get_variables('gen_deconv3') + net.get_variables('gen_deconv4') + net.get_variables('gen_deconv5')
+    dis_var = net.get_variables('dis_prob') + net.get_variables('dis_conv1') + net.get_variables('dis_conv2') + net.get_variables('dis_conv3') + net.get_variables('dis_conv4')
 
     net.set_cost_gen(0.5 * tf.reduce_mean((net.get_output('gen_dis') - 1)**2))
     net.set_optimizer_gen(tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(net.cost_gen, var_list= gen_var))
